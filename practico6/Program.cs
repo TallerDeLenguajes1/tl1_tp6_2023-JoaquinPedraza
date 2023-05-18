70b9ef0e261b610dbf9cb6fe9a6d72582c6fe311
JoaquinPedraza <joaquinpedraza162@gmail.com>
2023-05-18 19:37:27 -0300

    punto 1 completado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,9 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int intNum1 = 0;
+bool anda = false;
+string? numString = "";
 
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("valor de a:" + a);
-Console.WriteLine("valor de b:" + b);
+while (!anda)
+{
+  Console.WriteLine("Ingrese un número válido");
+  numString = Console.ReadLine();
+  anda = int.TryParse(numString, out intNum1);
+  if (!anda)
+  {
+    Console.WriteLine(numString + " no es número válido.");
+  }
+}
+
+if (intNum1 > 0)
+{
+  double numInvertido = 0;
+
+  while (intNum1 / 10 > 0)
+  {
+    numInvertido = numInvertido * 10 + intNum1 % 10;
+    intNum1 = intNum1 / 10;
+  }
+  numInvertido = numInvertido * 10 + intNum1 % 10;
+
+  Console.WriteLine("El numero invertido es: " + numInvertido);
+}
